448b345b644b46d07fee766325c628c80d51d7c2
Aleksander Bagdasarov <aabagdasarov@sberbank.ru>
2022-12-19 15:03:32 +0300

    explode

--- src/main/scala/SqlToJson.scala
+++ src/main/scala/SqlToJson.scala
@@ -1,3 +1,4 @@
+import org.apache.spark.sql.functions.{col, explode, first, monotonically_increasing_id, when}
 import org.apache.spark.sql.types.{DataTypes, DoubleType, LongType, StringType, StructType}
 import org.apache.spark.sql.{Row, SparkSession}
 
@@ -22,10 +23,10 @@ object SqlToJson {
 //    )
 
     var structureData = Seq(
-      Row(545863491123L, Map("intent,system_name" -> 20.42)),
-      Row(545863491124L, Map("intent,system_name" -> 20.42)),
-      Row(545863491125L, Map("intent,system_name" -> 20.42)),
-      Row(545863491126L, Map("intent,system_name" -> 20.42)),
+      Row(545863491123L, Map("A07.01591.SberSpasibo_turn_off" -> 20.42, "A23.01639.Exchange_of_coins_for_banknotes" -> 20.42)),
+      Row(545863491124L, Map("A23.01639.Exchange_of_coins_for_banknotes" -> 20.42)),
+      Row(545863491125L, Map("A02.01575.Reasons_for_loan_arrears" -> 20.42)),
+      Row(545863491126L, Map("A31.12685.Bankruptcy" -> 20.42, "brak_osnovnoy_infoquiz" -> 20.42)),
     )
 
     val mapType = DataTypes.createMapType(StringType,DoubleType)
@@ -38,6 +39,23 @@ object SqlToJson {
       spark.sparkContext.parallelize(structureData),arrayStructureSchema)
     mapTypeDF.printSchema()
     mapTypeDF.show()
+
+    val explodeDF = mapTypeDF.select($"epk_id", explode($"calibrated_score_array"))
+      .withColumn("system_name", when(col("key") === "A07.01591.SberSpasibo_turn_off", "SberSpasibo_turn_off")
+        when(col("key") === "A23.01639.Exchange_of_coins_for_banknotes", "Exchange_of_coins_for_banknotes")
+        when(col("key") === "A02.01575.Reasons_for_loan_arrears", "loans_general_info")
+        when(col("key") === "A31.12685.Bankruptcy", "Bankruptcy")
+        when(col("key") === "brak_osnovnoy_infoquiz", "gosuslugi")
+      )
+      .withColumnRenamed("key","intent")
+      .withColumnRenamed("value","score")
+//      .groupBy("epk_id")
+//      .pivot("key")
+//      .agg(first("value")).show()
+    explodeDF.printSchema()
+    explodeDF.show()
+
+
 
 //    //From Data (USING createDataFrame)
 //    val dfFromData2 = spark.createDataFrame(data).toDF(columns:_*)
